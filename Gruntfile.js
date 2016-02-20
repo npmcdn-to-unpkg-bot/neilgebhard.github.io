c1f8bc550c3ab5f3a6a1c376d669d9a477477f7e
neilgebhard <neilgebhard@gmail.com>
2016-02-20 12:37:02 +1000

    Add Gruntfile modifications, livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,53 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/script.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/script.min.js': ['<%= concat.dist.dest %>']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/script.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/* Updated on <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/script.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        watch: {
+            options: { livereload: true },
+            files: ['index.html', 'Gruntfile.js', 'src/**/*.js'],
+            tasks: []
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js']
+        },
+        express: {
+            all: {
+                options: {
+                    port: 5000,
+                    hostname: 'localhost',
+                    bases: ['./'],
+                    livereload: true
+                }
+            }
         }
-      }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['./index.html', '<%= jshint.files %>'],
-      tasks: ['jshint'],
-      options: {
-        livereload: true
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+  	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express');
 
-  grunt.registerTask('test', ['jshint']);
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('server', ['jshint', 'express', 'watch']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-My personal website
-=======
-# neilgebhard
-My personal website
->>>>>>> 2de1efe37bd2625adb68de7fbbb44fde03154dc5
+My personal website for experimental projects
