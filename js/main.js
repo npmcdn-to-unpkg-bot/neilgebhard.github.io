7ab60f7bd92fcd6c65dfcabdff6bd422ed940508
neilgebhard <neilgebhard@gmail.com>
2016-08-17 12:57:49 -0700

    Refactor JS and CSS

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// timesheet.js
+$(document).ready(function() {
+    var now = moment().format('MM/YYYY');
+    new Timesheet('timesheet', 2011, 2016, [
+        ['03/2011', '03/2014', 'Computer Science B.S.', 'ipsum'],
+        ['06/2013', '08/2013', 'Web Development Intern, Pacific Amusement Inc.', 'default'],
+        ['09/2013', '03/2014', 'iOS Development Intern, KidzPlan', 'default'],
+        ['09/2013', '03/2014', 'Professor Asstistant, Kristin Tufte', 'default'],
+        ['05/2014', now, 'Front-end Developer, Supportland', 'lorem'],
+        ['05/2016', now, 'Full Stack Developer, WelVU', 'dolor']
+    ]);
+
+    // typed.js
+    $("#typed-strings").typed({
+        strings: ['<span class="hero-title">Neil Gebhard</span>'],
+        contentType: 'html',
+        typeSpeed: 25
+    });
+
+    // aos.js
+    AOS.init();
+
+    // grid.js
+    var grid = new Minigrid({
+        container: '.cards',
+        item: '.card',
+        gutter: 6
+    });
+    grid.mount();
+});
